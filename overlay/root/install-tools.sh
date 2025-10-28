bfa955bd37f337883dae18849bb23be40f0f6612
Oliver <932481+tb-killa@users.noreply.github.com>
2025-10-28 09:55:12 +0100

    Add dmraid and mdadm to package installation

--- overlay/root/install-tools.sh
+++ overlay/root/install-tools.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 echo "Installiere benötigte Pakete (minimal)..."
-dnf install -y virt-v2v virtio-win qemu-img dialog hivex ntfs-3g curl NetworkManager iproute ipcalc
+dnf install -y virt-v2v virt-p2v dmraid mdadm lvm2 device-Mappe virtio-win qemu-img dialog hivex ntfs-3g curl NetworkManager iproute ipcalc
 systemctl enable NetworkManager
 systemctl enable migration-autostart.service
 # Standard-Root-Passwort:
